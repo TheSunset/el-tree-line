304919f33755f04b72c3080731236e4369ce1913
snowsunset <snow@sunset.com>
2025-08-16 10:24:22 +0800

    feat: 更新

--- vite.config.ts
+++ vite.config.ts
@@ -15,10 +15,8 @@ export default defineConfig({
       cssFileName: "index",
     },
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖,解决插件报错问题(reading 'isCE')
       external: ["vue"],
       output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: "Vue",
         },
